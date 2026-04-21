de4cae07cdea8f1ffb2fd2a35ec4a2eae9bd1503
AnthonyVerdiPhoto6 <from.the.pit.anthony@gmail.com>
2026-04-21 17:41:05 -0400

    Add arist
    Added Baby Keem example

--- lib/albums.ts
+++ lib/albums.ts
@@ -9,6 +9,7 @@ const ALBUM_DISPLAY_NAMES: Record<string, string> = {
   "jid-yung-nudy": "JID & Yung Nudy",
   "lorde-2hollis": "Lorde & 2hollis",
   tokischa: "Tokischa",
+  "baby-keem": "Baby Keem",
 };
 
 export type AlbumSummary = {
