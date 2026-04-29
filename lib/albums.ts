5c3eb179c99861a12846bbafad1bc27209540a99
AnthonyVerdiPhoto6 <from.the.pit.anthony@gmail.com>
2026-04-29 19:42:45 -0400

    Update albums.ts

--- lib/albums.ts
+++ lib/albums.ts
@@ -9,6 +9,7 @@ const ALBUM_DISPLAY_NAMES: Record<string, string> = {
   "lorde-2hollis": "Lorde & 2hollis",
   tokischa: "Tokischa",
   "baby-keem": "Baby Keem",
+  "pinkpantheress": "PinkPantheress",
 };
 
 export type AlbumSummary = {
